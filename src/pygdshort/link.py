6d171142a7132213db1889011727f4e0b26ab160
TheOctoGirl <119755793+TheOctoGirl@users.noreply.github.com>
2023-10-27 20:39:33 -0700

    Add URL expansion

--- src/pygdshort/link.py
+++ src/pygdshort/link.py
@@ -1,7 +1,16 @@
 import requests
 
-def shorten(long_url:str, custom_short_url: str=None):
+def shorten(long_url:str, custom_short_url: str = None):
     
+    """Shortens a URL using the is.gd API.
+    
+    Parameters:
+    
+        long_url: The URL to be shortened.
+
+        custom_short_url: The custom short URL to be used. (Optional)
+    """
+
     if custom_short_url != None:
         parameters = {
             "url": long_url,
@@ -35,6 +44,40 @@ def shorten(long_url:str, custom_short_url: str=None):
         
     else:
         return shortened_url["shorturl"]
+    
+
+def expand(short_url:str):
+    
+    """Expands a shortened URL using the is.gd API.
+    
+    Parameters:
+
+        short_url: The shortened URL to be expanded.
+    """
+    
+    parameters = {
+        "shorturl": short_url,
+        "format": "json"
+    }
+    expanded_url = requests.get("https://is.gd/forward.php", params=parameters)
+    expanded_url = expanded_url.json()
+
+    if "errorcode" in expanded_url:
+        
+        if expanded_url["errorcode"] == 1:
+            raise LongUrlError(expanded_url["errormessage"])
+        
+        elif expanded_url["errorcode"] == 2:
+            raise ShortUrlError(expanded_url["errormessage"])
+        
+        elif expanded_url["errorcode"] == 3:
+            raise RateLimitError(expanded_url["errormessage"])
+        
+        else:
+            raise GenericError(expanded_url["errormessage"])
+        
+    else:
+        return expanded_url["url"]
 
 
 class LongUrlError(Exception):
